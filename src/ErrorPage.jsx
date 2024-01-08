46533ab827141a54311530ffa84c246f7382957f
Saimum-14028 <md.amiralfahim@gmail.com>
2024-01-08 22:53:11 +0600

    Toggle theme  added  alhamdulillah

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div className="h-screen w-screen bg-gray-50 flex items-center">
+        <div className="w-screen flex items-center py-10">
             <div className="container flex flex-col md:flex-row items-center justify-between px-5 text-gray-700">
                     <div className="w-full lg:w-1/2 mx-8">
                         <div className="text-7xl text-green-500 font-dark font-extrabold mb-8"> 404</div>
