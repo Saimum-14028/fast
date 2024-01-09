fc92b51199b1fec7c4daadffe29209b32ab7a5c3
Saimum-14028 <md.amiralfahim@gmail.com>
2024-01-09 20:56:11 +0600

    tanstack query and materual ui added  alhamdulillah

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,18 @@ import { RouterProvider } from 'react-router-dom'
 import myCreatedRoute from './Route.jsx'
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './AuthProvider.jsx'
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <HelmetProvider>
       <AuthProvider>
         <RouterProvider router={myCreatedRoute}></RouterProvider>
       </AuthProvider>
     </HelmetProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
